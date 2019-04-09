bf4afa8a882882af4de0ea45f50cbf1d84e6c1e8
ramonpereirat <ramontorres15@yahoo.com.br>
2019-04-09 19:32:33 -0300

    Versao demorada

---
+++ src/testar/Aluno.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testar;
+
+/**
+ *
+ * @author ifnmg
+ */
+public class Aluno {
+    
+    String alunos;
+    String idade;
+    String sexo;
+    
+}
---
+++ src/testar/Testar.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testar;
+
+/**
+ *
+ * @author ifnmg
+ */
+public class Testar {
+    
+    public static void main(String[] args){
+    
+    
+    Aluno A1 = new Aluno();
+    A1.alunos = "Ramon Torres";  
+    System.out.println("Aluno " +A1.alunos);
+    
+    
+    Professor P1 = new Professor();
+    P1.professores = "X";  
+    System.out.println("Professor " +P1.professores);
+    
+    
+    Aula Al1 = new Aula();
+    Al1.aulas = "Programacao oreientada a objeto"; 
+    System.out.println("Aulas " +Al1.aulas);
+    } 
+      
+}
+
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
